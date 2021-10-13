8fcfa8f11f8419ed134a22ab9ad044df75c048bb
Carlos <mezquitalobato@gmail.com>
2021-10-13 13:11:36 +0200

    Main file modified

--- 01-HelloWorld/src/com/liceolapaz/des/jprf/Main.java
+++ 01-HelloWorld/src/com/liceolapaz/des/jprf/Main.java
@@ -6,6 +6,7 @@ public class Main {
     public static void main(String [] args){
         System.out.println("Hello World!");
         System.out.println("The shortcut for System.out.println is sout");
+        System.out.println("And CTRL+J to check all the shortcuts");
 
     }
 }
