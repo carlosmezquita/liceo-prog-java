e4af0f8b8854f9842d56dcd685fac1a968268b4a
Carlos <mezquitalobato@gmail.com>
2021-10-13 13:10:10 +0200

    Main file modified

--- 01-HelloWorld/src/com/liceolapaz/des/jprf/Main.java
+++ 01-HelloWorld/src/com/liceolapaz/des/jprf/Main.java
@@ -1,7 +1,11 @@
 package com.liceolapaz.des.jprf;
 
+import java.time.Clock;
+
 public class Main {
     public static void main(String [] args){
         System.out.println("Hello World!");
+        System.out.println("The shortcut for System.out.println is sout");
+
     }
 }
