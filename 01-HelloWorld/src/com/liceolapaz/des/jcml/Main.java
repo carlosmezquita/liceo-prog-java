bf6c45bf46919e55d8bb70f0d9d18ee4311680d1
Carlos <mezquitalobato@gmail.com>
2021-10-13 13:22:09 +0200

    Main file modified

--- 01-HelloWorld/src/com/liceolapaz/des/jcml/Main.java
+++ 01-HelloWorld/src/com/liceolapaz/des/jcml/Main.java
@@ -1,9 +1,8 @@
-package com.liceolapaz.des.jprf;
-
-import java.time.Clock;
+package com.liceolapaz.des.jcml;
 
 public class Main {
     public static void main(String [] args){
+
         System.out.println("Hello World!");
         System.out.println("The shortcut for System.out.println is sout");
         System.out.println("And CTRL+J to check all the shortcuts");
